809dab6d9bc7ab7987e02b19d2cec26d803e85ad
fahriza chromebook <kurniawanfahriza@gmail.com>
2025-06-30 18:45:22 +0700

    sederhanakan database

--- src/app/(admin)/admin/products/[id]/edit/page.jsx
+++
@@ -1,5 +0,0 @@
-import ProductForm from "../../ProductForm";
-
-export default function EditProductPage({ params }) {
-  return <ProductForm productId={params.id} />;
-}
--- src/app/(admin)/admin/products/create/page.jsx
+++
@@ -1,5 +0,0 @@
-import ProductForm from "../ProductForm";
-
-export default function CreateProductPage() {
-  return <ProductForm />;
-}
